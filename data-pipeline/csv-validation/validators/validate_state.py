e43c46bec43f50521a3d9c1e62c4e003c60c7efd
Kobe <kobetbuckley@gmail.com>
2023-10-10 20:51:41 -1000

    checking for state as a word

--- data-pipeline/csv-validation/validators/validate_state.py
+++ data-pipeline/csv-validation/validators/validate_state.py
@@ -4,5 +4,7 @@ from exceptions import InvalidStateException
 def validate_state(val):
     
     if val != "HI":
-        val = 'REPLACE_TEST'
         raise InvalidStateException("THIS IS TESTING THE STATE VALIDATION REPLACEMENT PLAN")
+    
+    if val == "State":
+        raise InvalidStateException("WHY DOES VALUE SAY STATE HERE")
