66ec635e0debbc7d2c94b4138450b7048bb99922
Kobe <kobetbuckley@gmail.com>
2023-10-14 14:02:18 -1000

    cleanup

--- data-pipeline/csv-validation/validators/validate_state.py
+++ data-pipeline/csv-validation/validators/validate_state.py
@@ -4,4 +4,4 @@ from exceptions import InvalidStateException
 def validate_state(val):
     
     if val != "HI":
-        raise InvalidStateException("THIS IS TESTING THE STATE VALIDATION REPLACEMENT PLAN")
+        raise InvalidStateException("Invalid State. State should be HI.")
