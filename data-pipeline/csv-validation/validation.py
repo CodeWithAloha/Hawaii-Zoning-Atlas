db03ed4252d2e43d45fd42a071ea0e0127c97a02
Michael Tamaki <michaeltamakidev@gmail.com>
2024-03-11 18:54:42 -1000

    Correctly handle new lines in CSV validation script

--- data-pipeline/csv-validation/validation.py
+++ data-pipeline/csv-validation/validation.py
@@ -124,7 +124,7 @@ constraints = [
     ),
 ]
 
-table_full = etl.fromcsv("../hawaii-zoning-data.csv")
+table_full = etl.fromcsv("../hawaii-zoning-data.csv", newline='')
 table = etl.tail(table_full, table_full.len() - 2)
 problems = etl.validate(table, constraints=constraints, header=headers)
 if problems.len() > 2:
